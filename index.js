4bd94d77b9064ea9e17559cb895f3c462c875a41
Theo Malaper <theomalaper.cognez@gmail.com>
2019-07-06 03:13:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {}
 
 function udpateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign(object, {[key]: value})
+  return Object.assign({}, [object], {[key]: value})
 }
